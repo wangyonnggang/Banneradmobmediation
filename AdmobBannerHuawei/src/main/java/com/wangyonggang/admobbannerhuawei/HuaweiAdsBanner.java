87fa95f997c7e54b855d0f6d4cedf8357dddff49
wangyonggang <wangyonggang199702@gmail.com>
2022-02-07 14:43:03 +0800

    Initial commit

---
+++ AdmobBannerHuawei/src/main/java/com/wangyonggang/admobbannerhuawei/HuaweiAdsBanner.java
@@ -0,0 +1,79 @@
+package com.wangyonggang.admobbannerhuawei;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.mediation.MediationAdRequest;
+import com.google.android.gms.ads.mediation.customevent.CustomEventBanner;
+import com.google.android.gms.ads.mediation.customevent.CustomEventBannerListener;
+import com.huawei.hms.ads.AdParam;
+import com.huawei.hms.ads.BannerAdSize;
+import com.huawei.hms.ads.banner.BannerView;
+
+public class HuaweiAdsBanner implements CustomEventBanner{
+    /** The SampleAdView representing a banner ad. */
+    private BannerView bannerView ;
+
+    /** The event is being destroyed. Perform any necessary cleanup here. */
+    @Override
+    public void onDestroy() {
+        if (bannerView != null) {
+            bannerView.destroy();
+        }
+    }
+
+    /**
+     * The app is being paused. This call is only forwarded to the adapter if
+     * the developer notifies AdMob mediation that
+     * the app is being paused.
+     */
+    @Override
+    public void onPause() {
+        // The sample ad network doesn't have an onPause method, so does nothing.
+    }
+
+    /**
+     * The app is being resumed. This call is only forwarded to the
+     * adapter if the developer notifies AdMob
+     * mediation that the app is being resumed.
+     */
+    @Override
+    public void onResume() {
+        // The sample ad network doesn't have an onResume method, so does nothing.
+    }
+
+    @Override
+    public void requestBannerAd(Context context,
+                                CustomEventBannerListener listener,
+                                String serverParameter,
+                                AdSize size,
+                                MediationAdRequest mediationAdRequest,
+                                Bundle customEventExtras) {
+
+        bannerView = new BannerView(context);
+
+        // Assumes that the serverParameter is the AdUnit for the Sample Network.
+       bannerView.setAdId(serverParameter);
+
+        bannerView.setBannerAdSize(new BannerAdSize(size.getWidth(), size.getHeight()));
+
+        // Implement a SampleAdListener and forward callbacks to AdMob.
+        // The callback forwarding is handled by SampleBannerEventForwarder.
+      bannerView.setAdListener(new SampleCustomBannerEventForwarder(listener, bannerView));
+
+        //AdParam adParam = new AdParam.Builder().build();
+        // Make an ad request.
+        //bannerView.loadAd(adParam);
+        bannerView.loadAd(createSampleRequest(mediationAdRequest));
+
+    }
+    private AdParam createSampleRequest(MediationAdRequest mediationAdRequest) {
+        AdParam request = new AdParam.Builder().build();
+        //SampleAdRequest request = new SampleAdRequest();
+
+        return request;
+    }
+
+
+}
---
+++ AdmobBannerHuawei/src/main/java/com/wangyonggang/admobbannerhuawei/SampleCustomBannerEventForwarder.java
@@ -0,0 +1,62 @@
+package com.wangyonggang.admobbannerhuawei;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.mediation.customevent.CustomEventBannerListener;
+import com.huawei.hms.ads.AdListener;
+import com.huawei.hms.ads.banner.BannerView;
+
+public class SampleCustomBannerEventForwarder extends AdListener {
+    private CustomEventBannerListener mBannerListener;
+    private BannerView mAdView;
+
+    /**
+     * Creates a new {@code SampleBannerEventForwarder}.
+     * @param listener A {@link CustomEventBannerListener} that should receive
+     *                 forwarded events.
+     * @param adView   A {@link }.
+     */
+    public SampleCustomBannerEventForwarder(
+            CustomEventBannerListener listener, BannerView adView) {
+        this.mBannerListener = listener;
+        this.mAdView = adView;
+    }
+
+    @Override
+    public void onAdLoaded(){
+        mBannerListener.onAdLoaded(mAdView);
+    }
+
+    @Override
+    public void onAdFailed(int errorCode) {
+        switch (errorCode) {
+            case 0:
+                mBannerListener.onAdFailedToLoad(AdRequest.ERROR_CODE_INTERNAL_ERROR);
+                break;
+            case 1:
+                mBannerListener.onAdFailedToLoad(AdRequest.ERROR_CODE_INVALID_REQUEST);
+                break;
+            case 2:
+                mBannerListener.onAdFailedToLoad(AdRequest.ERROR_CODE_NETWORK_ERROR);
+                break;
+            case 3:
+                mBannerListener.onAdFailedToLoad(AdRequest.ERROR_CODE_NO_FILL);
+                break;
+        }
+    }
+
+    @Override
+    public void onAdOpened() {
+        mBannerListener.onAdClicked();
+        mBannerListener.onAdOpened();
+    }
+    @Override
+    public void onAdClicked() {
+        // 广告点击时调用
+        mBannerListener.onAdClicked();
+
+    }
+    @Override
+    public void onAdClosed() {
+        mBannerListener.onAdClosed();
+    }
+}
